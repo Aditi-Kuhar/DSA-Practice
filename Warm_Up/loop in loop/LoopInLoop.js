d1c25455b9a9f2b01f694fe0836c952a8ce57de8
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-06-23 11:22:01 +0530

    loopinloop

---
+++ Warm_Up/loop in loop/LoopInLoop.js
@@ -0,0 +1,36 @@
+for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+        console.log("i="+ i + " j=" +j);
+    }
+}
+console.log("\n");
+for(let i=0;i<3;i++){
+    for(let j=0;j<i;j++){
+        console.log(i + " " +j);
+    }
+}
+console.log("\n");
+for(let i=0;i<5;i++){
+    for(let j=0;j<=i;j++){
+        console.log("i hai="+ i + " j hai=" +j);
+    }
+}
+console.log("\n");
+for(let i=0;i<3;i++){
+    for(let j=i;j>0;j--){
+        console.log(i + " " +j);
+    }
+}
+console.log("\n");
+for(let i=0;i<3;i++){
+    for(let j=i;j>=0;j--){
+        console.log("i="+ i + " j=" +j);
+    }
+}
+console.log("\n");
+for(let i=3;i>0;i--){
+    for(let j=0;j<i;j++){
+    console.log(i + " " +j);
+
+    }
+}
