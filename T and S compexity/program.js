f993befca6708e1dfd7b4c4914c152e89aba16cd
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-06-26 13:52:59 +0530

    t and s complexity

---
+++ T and S compexity/program.js
@@ -0,0 +1,3 @@
+// Time complexity-It is the measure of efficiency of an algorithm in terms of speed ,as input size grows
+// O(1)>O(logn)>O(n)>O(nlogn)>O(n**2)>O(n**3)>O(2**n)>O(n!)
+//space compexity-It is the measure of extra space taken by an algorithm
