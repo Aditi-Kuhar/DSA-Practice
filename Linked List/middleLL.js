6ba72361b2cc429d028c54eaa6231dd7b8ac8a0f
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-07-11 14:48:37 +0530

    middle and reverse LL

---
+++ Linked List/middleLL.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let arr = [];
+    let curr=head;
+    while(curr!=null){
+        arr.push(curr);
+        curr=curr.next;
+    }
+    let n=Math.floor(arr.length/2);
+    return arr[n];
+};
