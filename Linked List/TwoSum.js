47873388ad7db33e628d035eaa5fb080f8410e16
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-07-21 23:57:25 +0530

    Two sum LL

--- Linked List/TwoSum.js
+++ Linked List/TwoSum.js
@@ -124,3 +124,22 @@ var addTwoNumbers = function(l1, l2) {
 
     return sentinel.next;
 };
+
+//sir(ternary  operator) ka use krke code ko aur chota kr skte h
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let curr = dummy;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+        carry = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+
+    return dummy.next;
+};
+    
