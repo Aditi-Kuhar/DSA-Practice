f38eaefae89dbecfe2b3674287ad9ac905129b08
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-08-02 21:45:28 +0530

    lengthofLastWord

---
+++ Strings/lengthOfLastWord.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) 
+{
+    let n=s.length-1;
+    let count=0;
+    for(let i=n;i>=0;i--)
+    {
+        if(s[i]!=" ") count++;
+        else if (count>0) break;
+    }
+    return count;
+};
