37dd1d61b09cdfa31ba591fba591b845101fb2cd
Aditi Kuhar <aditikuhar6317@gmail.com>
2026-08-08 00:28:48 +0530

    valid palindrome

--- Strings/validPalindrome.js
+++ Strings/validPalindrome.js
@@ -1,3 +1,24 @@
+//sir 
+var isPalindrome = function (s) {
+s = s.toLowerCase();
+let filteredString = "";
+let rev = "";
+for (let i = 0; i < s.length; i++) {
+    if (
+    (s[i].charCodeAt() >= "a".charCodeAt() && s[i].charCodeAt() <= "z".charCodeAt()) ||
+    (s[i].charCodeAt() >= "0".charCodeAt() && s[i].charCodeAt() <= "9".charCodeAt())
+    ) {
+    filteredString = filteredString + s[i];
+    rev = s[i] + rev;
+    }
+}
+return filteredString === rev;
+};
+
+//TC=O(n) where n is the length of the string s
+//SC=O(n) because we are using extra space for filteredString and rev
+
+
 /**
  * @param {string} s
  * @return {boolean}
@@ -17,3 +38,6 @@ var isPalindrome = function(s)
     }
     return true;
 };
+
+//TC=O(n) where n is the length of the string s
+//SC=O(1) because we are using two pointers and not using any extra space for data structures
